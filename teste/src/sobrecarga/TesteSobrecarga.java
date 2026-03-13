95f9670cf651b78902356211ed8aa993debadb95
Guilherme Dias Batista <106848620+gl-dias@users.noreply.github.com>
2026-03-13 16:48:59 -0300

    Refactor method calls to use 'somar' in TesteSobrecarga

--- teste/src/sobrecarga/TesteSobrecarga.java
+++ teste/src/sobrecarga/TesteSobrecarga.java
@@ -3,9 +3,9 @@ package sobrecarga;
 public class TesteSobrecarga {
     private static void main(final String[] args) {
         Calculadora calc = new Calculadora();
-        System.out.println(calc.somarDoisInteiros(2, 1)); // Chama o 1° método
-        System.out.println(calc.somarTresInteiros(2, 1, 2)); // Chama o 2°método
-        System.out.println(calc.somarDecimais(2.0, 1.0)); // Chama o 3° método
+        System.out.println(calc.somar(2, 1)); // Chama o 1° método
+        System.out.println(calc.somar(2, 1, 2)); // Chama o 2°método
+        System.out.println(calc.somar(2.0, 1.0)); // Chama o 3° método
 
     }
 }
